77405862bec07836b904ee6ad247b8cacd112ef0
김태현 <142918648+Lunamana00@users.noreply.github.com>
2024-11-10 06:14:01 +0900

    [FEAT] Finish melee

--- Alpha/Source/Alpha/AIC_Enemy_1.cpp
+++ Alpha/Source/Alpha/AIC_Enemy_1.cpp
@@ -1,33 +1,25 @@
-
-
 #include "AIC_Enemy_1.h"
 #include "Kismet/GameplayStatics.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Actor.h"
+#include "AIController.h"
+#include "NavigationSystem.h"
 
 void AAIC_Enemy_1::BeginPlay() {
+    Super::BeginPlay();
 
-	Super::BeginPlay();
-	APawn* PlayerPawn = UGameplayStatics::UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-	GetBlackboardComponent()->SetValueAsObject(TEXT("Player"), PlayerPawn);
-
-
-
-
-
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    GetBlackboardComponent()->SetValueAsObject(TEXT("Player"), PlayerPawn);
 }
 
 void AAIC_Enemy_1::Tick(float DeltaSeconds) {
+    Super::Tick(DeltaSeconds);
 
-	Super::Tick(DeltaSeconds);
-	APawn* PlayerPawn = UGameplayStatics::UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-
-	GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
-	GetBlackboardComponent()->SetValueAsVector(TEXT("ActorLocation"), GetPawn()->GetActorLocation());
-
-
-	float Distance = FVector::Dist(PlayerPawn->GetActorLocation(), GetPawn()->GetActorLocation());
-	GetBlackboardComponent()->SetValueAsFloat(TEXT("Distance"), Distance);
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+    GetBlackboardComponent()->SetValueAsVector(TEXT("ActorLocation"), GetPawn()->GetActorLocation());
 
 
-
+    float Distance = FVector::Dist(PlayerPawn->GetActorLocation(), GetPawn()->GetActorLocation());
+    GetBlackboardComponent()->SetValueAsFloat(TEXT("Distance"), Distance);
 }
