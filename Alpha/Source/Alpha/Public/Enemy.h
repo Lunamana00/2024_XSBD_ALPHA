ac6659c0657f8f2c54bb534cfdb2979284a1b014
김태현 <142918648+Lunamana00@users.noreply.github.com>
2024-12-30 12:30:50 +0900

    [FEAT] Refactoring Structure

--- Alpha/Source/Alpha/Public/Enemy.h
+++ Alpha/Source/Alpha/Public/Enemy.h
@@ -4,43 +4,57 @@
 #include "GameFramework/Character.h"
 #include "Enemy.generated.h"
 
+/*
+적 기본 클래스
+ */
+
+UENUM(BlueprintType)
+enum class EEnemyType : uint8
+{
+	None UMETA(DisplayName = "None"),
+	Flight UMETA(DisplayName = "Flight"),
+	Melee UMETA(DisplayName = "Melee"),
+	Ranged UMETA(DisplayName = "Ranged")
+};
+
 UCLASS()
 class ALPHA_API AEnemy : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// 생성자
 	AEnemy();
 
 protected:
-	// 게임 시작 또는 스폰 시 호출
 	virtual void BeginPlay() override;
 
 public:
-	// 매 프레임 호출
 	virtual void Tick(float DeltaTime) override;
 
-	// 적의 체력
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Stats")
+	//체력
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Info")
 	float Health;
 
-	// 적의 최대 체력
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Stats")
+	//최대 체력
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Info")
 	float MaxHealth;
 
-	// 적의 이동 속도
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Stats")
+	//이동 속도
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Info")
 	float MovementSpeed;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Stats")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Info")
 	float Damage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Info")
+	EEnemyType EnemyType;
 	
 	// 데미지를 받았을 때 호출
 	UFUNCTION(BlueprintCallable, Category = "Enemy Event")
 	void TakeDamage(float DMG);
 
-	// 적의 사망 처리
+	//사망 처리
 	UFUNCTION(BlueprintCallable, Category = "Enemy Event")
 	void Die();
+
 };
